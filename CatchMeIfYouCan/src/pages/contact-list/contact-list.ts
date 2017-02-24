010f20729a6aaf4ab61b60141ee48fd49306e9a0
Adam Ben Shmuel <bsadambs@gmail.com>
2017-02-24 16:41:26 +0200

    Create empty contact list page.

---
+++ CatchMeIfYouCan/src/pages/contact-list/contact-list.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by AdamBS on 2/24/2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-contact-list-ionic',
+  templateUrl: 'contact-list.html'
+})
+export class ContactListPage {
+  constructor() {
+
+  }
+}
