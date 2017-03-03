74feb935d93a94b80ce86ef6f05888167f0637f0
Adam Ben Shmuel <bsadambs@gmail.com>
2017-03-03 14:57:07 +0200

    Configure Facebook Login pt2

--- CatchMeIfYouCan/src/app/app.component.ts
+++ CatchMeIfYouCan/src/app/app.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { Platform, MenuController, Nav } from 'ionic-angular';
-
-import { StatusBar, Splashscreen, Geolocation } from 'ionic-native';
-
-import { FacebookService } from '../services/facebook.service/facebook.service'
+import { StatusBar, Splashscreen, Geolocation, Facebook, NativeStorage } from 'ionic-native';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ListPage } from '../pages/list/list';
 import { ContactListPage } from '../pages/contact-list/contact-list';
-import { LoginPage } from '../pages/login-page/login-page';
+import {LoginPage} from "../pages/login-page/login-page";
 
 @Component({
-  templateUrl: 'app.html',
-  providers: [FacebookService]
+  templateUrl: 'app.html'
 })
-export class MyApp implements OnInit {
+export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
@@ -26,19 +22,24 @@ export class MyApp implements OnInit {
     public platform: Platform,
     public menu: MenuController
   ) {
-    this.initializeApp();
+    let env = this;
+    NativeStorage.getItem('user')
+      .then(function (data){
+        env.nav.push(HelloIonicPage);
+        Splashscreen.hide();
+      }, function(error) {
+        env.nav.push(LoginPage);
+        Splashscreen.hide();
+      });
 
-    // set our app's pages
-    this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage },
-      { title: 'Login Page', component: LoginPage },
-      { title: 'Contact List', component: ContactListPage }
-    ];
-  }
-
-  ngOnInit(){
-    this._facebookService.loadAndInitFBSDK();
+    StatusBar.styleDefault();
+    //
+    // // set our app's pages
+    // this.pages = [
+    //   { title: 'Hello Ionic', component: HelloIonicPage },
+    //   { title: 'My First List', component: ListPage },
+    //   { title: 'Contact List', component: ContactListPage }
+    // ];
   }
 
   initializeApp() {
@@ -47,22 +48,6 @@ export class MyApp implements OnInit {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
-    });
-  }
-
-  login(){
-    var self = this;
-    FB.login(function(response) {
-      if (response.authResponse) {
-        FB.api('/me', function(response) {
-          self._ngZone.run(() => {
-            self.name = response.name;
-            self.isUser = true
-          });
-        });
-      }else{
-        console.log('User cancelled login or did not fully authorize.');
-      }
     });
   }
 
