323fa1142278d31ab6bff79f3ecf245baa645004
Adam Ben Shmuel <bsadambs@gmail.com>
2017-03-04 17:48:46 +0200

    Handle user entering app.

--- CatchMeIfYouCan/src/app/app.component.ts
+++ CatchMeIfYouCan/src/app/app.component.ts
@@ -12,29 +12,37 @@ import {LoginPage} from "../pages/login-page/login-page";
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
 
-  // make HelloIonicPage the root (or first) page
-  rootPage: any = HelloIonicPage;
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any;
+
   pages: Array<{title: string, component: any}>;
 
   constructor(
     public platform: Platform,
     public menu: MenuController
   ) {
-    let env = this;
-    NativeStorage.getItem('user')
-      .then(function (data){
-        env.nav.push(HelloIonicPage);
-        Splashscreen.hide();
-      }, function(error) {
-        env.nav.push(LoginPage);
-        Splashscreen.hide();
-      });
+    platform.ready().then(() => {
 
-    StatusBar.styleDefault();
+      // Here we will check if the user is already logged in
+      // because we don't want to ask users to log in each time they open the app
+      let env = this;
+      NativeStorage.getItem('user')
+        .then(function (data){
+          // user is previously logged and we have his data
+          // we will let him access the app
+          env.nav.push(HelloIonicPage);
+          Splashscreen.hide();
+        }, function(error) {
+          //we don't have the user data so we will ask him to log in
+          env.nav.push(LoginPage);
+          Splashscreen.hide();
+        });
+
+      StatusBar.styleDefault();
+    });
     //
-    // // set our app's pages
+    // set our app's pages
     // this.pages = [
     //   { title: 'Hello Ionic', component: HelloIonicPage },
     //   { title: 'My First List', component: ListPage },
